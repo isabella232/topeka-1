e9cf9be462134d642dd0330ead0decf23e06cdcf
Michal Mocny <mmocny@gmail.com>
2014-08-25 15:54:10 -0400

    Adding chrome.notifications

--- background.js
+++ background.js
@@ -1,12 +1,38 @@
-/**
- * Listens for the app launching then creates the window.
- * Ignores the provided window size.
- *
- * @see http://developer.chrome.com/trunk/apps/app.window.html
- */
+'use strict';
+
+/******************************************************************************/
+
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     width: 244,
     height: 380,
   });
 });
+
+/******************************************************************************/
+
+chrome.notifications.onClosed.addListener(function(notificationId) {
+  console.log('onClosed fired. notificationId =', notificationId);
+});
+
+chrome.notifications.onClicked.addListener(function(notificationId) {
+  console.log('onClicked fired. notificationId =', notificationId);
+
+  chrome.notifications.clear(notificationId, function(wasCleared) {
+  });
+});
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
+  console.log('onButtonClicked fired. notificationId =', notificationId, ', buttonIndex =', buttonIndex);
+});
+
+/******************************************************************************/
+
+function createLocalNotification(notificationId, options, callback) {
+  options.iconUrl = options.iconUrl || '';
+  options.message = options.message || 'Eh';
+  callback = callback || function() {}
+  chrome.notifications.create(notificationId, options, callback);
+}
+
+/******************************************************************************/
